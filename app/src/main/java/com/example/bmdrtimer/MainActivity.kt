c71b536dbdf5b19d560f15cf5b94acceb2bb41aa
MintaeKim <gg04253@gmail.com>
2023-08-01 18:35:47 +0900

    Update MainActivity.kt : 타이머 구현.

--- app/src/main/java/com/example/bmdrtimer/MainActivity.kt
+++ app/src/main/java/com/example/bmdrtimer/MainActivity.kt
@@ -11,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     private val remainMinTextView: TextView by lazy {
         findViewById(R.id.remainMinTextView);
     }
+    private val remainSecTextView: TextView by lazy {
+        findViewById(R.id.secondTextView);
+    }
     private val seekBar: SeekBar by lazy {
         findViewById(R.id.seekBar);
     }
@@ -20,16 +23,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    remainMinTextView.text = "%02d".format(progress);
+                }
 
-    private fun creatCountDownTimer(initialMillis:Long):CountDownTimer{
-        return  object : CountDownTimer(initialMillis,1000L){
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar ?:return
+                    creatCountDownTimer(seekBar.progress * 60 * 1000L)
+                }
+            }
+        )
+    }
+
+    private fun creatCountDownTimer(initialMillis: Long): CountDownTimer {
+        return object : CountDownTimer(initialMillis, 1000L) {
             override fun onTick(millisUntilFinished: Long) {
-
+                updateRemainTime(millisUntilFinished)
+                updateSeekbar(millisUntilFinished)
             }
 
             override fun onFinish() {
-                TODO("Not yet implemented")
+                updateRemainTime(0)
+                updateSeekbar(0)
             }
         }
+    }
+
+    private fun updateRemainTime(remainMilles: Long) {
+        val remainseconds = remainMilles / 1000
+
+        remainMinTextView.text = "%02".format(remainseconds / 60)
+        remainSecTextView.text = "%02".format(remainseconds % 60)
+
+    }
+
+    private fun updateSeekbar(remainMilles: Long) {
+        seekBar.progress = (remainMilles / 1000 / 60).toInt()
     }
 }
